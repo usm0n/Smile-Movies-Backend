9cb5f2ef9566482b1ddb7dd04b4cb15fd6afc632
Usmon <usmon.umarovich.09@gmail.com>
2024-06-11 19:09:48 +0500

    movieSchema changed from mongodb to nedb

--- services/movie.service.js
+++ services/movie.service.js
@@ -1,248 +1,286 @@
-import mongoose from "mongoose";
-import Movie from "../schemas/movie.schema.js";
 import Datastore from "nedb";
 
 export const movies = new Datastore({ filename: "./data/movies.json" });
 movies.loadDatabase((err) => console.log(err));
 
-export const getAllMovies = async (req, res) => {
+export const getAllMovies = (req, res) => {
   try {
-    await Movie.find().then((movies) => {
-      if (!movies || movies.length === 0) {
+    movies.find({}, (err, movies) => {
+      if (!movies) {
         res.status(404).json({
           message: "Movies not found",
         });
       } else {
-        res.status(200).json(movies);
-      }
-    });
-  } catch (error) {
-    res.status(500).json({
-      error: error.message,
-    });
-  }
-};
-export const getMovieById = async (req, res) => {
-  try {
-    await Movie.findById(req.params.id).then((movie) => {
-      if (!movie) {
-        res.status(404).json({
-          message: "Movie not found",
+        res.status(200).json({
+          movies: movies,
         });
-      } else {
-        res.status(200).json(movie);
       }
     });
   } catch (error) {
-    res.status(500).json({
-      error: error.message,
-    });
+    res.status(500).json({ error: error.message });
   }
 };
 
-export const updateMovieById = async (req, res) => {
+export const getMovieById = (req, res) => {
   try {
-    await Movie.findByIdAndUpdate(req.params.id, req.body).then((movie) => {
+    const id = req.params.id;
+    movies.findOne({ _id: id }, (err, movie) => {
       if (!movie) {
         res.status(404).json({
           message: "Movie not found",
         });
       } else {
-        res.status(200).json(movie);
+        res.status(200).json({
+          movie: movie,
+        });
       }
     });
   } catch (error) {
-    res.status(500).json({
-      error: error.message,
-    });
+    res.status(500).json({ error: error.message });
   }
 };
 
-export const deleteMovieById = async (req, res) => {
+export const updateMovieById = (req, res) => {
   try {
-    await Movie.findByIdAndDelete(req.params.id).then((movie) => {
+    const id = req.params.id;
+    const movie = req.body;
+    movies.update({ _id: id }, { $set: movie }, (err, movie) => {
       if (!movie) {
         res.status(404).json({
           message: "Movie not found",
         });
       } else {
-        res.status(200).json(movie);
+        res.status(200).json({
+          movie: movie,
+        });
       }
     });
   } catch (error) {
-    res.status(500).json({
-      error: error.message,
-    });
+    res.status(500).json({ error: error.message });
   }
 };
-export const deleteAllMovies = async (req, res) => {
+
+export const deleteMovieById = (req, res) => {
   try {
-    const movies = Movie.find({});
-    await Movie.deleteMany().then(() => {
-      if (!movies || !movies.length) {
+    const id = req.params.id;
+    movies.remove({ _id: id }, (err, movie) => {
+      if (!movie) {
+        res.status(404).json({
+          message: "Movie not found",
+        });
+      } else {
+        res.status(200).json({
+          movie: movie,
+        });
+      }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const deleteAllMovies = (req, res) => {
+  try {
+    movies.remove({}, (err, movie) => {
+      if (!movie) {
         res.status(404).json({
           message: "Movies not found",
         });
       } else {
-        res.status(200).json({ message: "All Movies are deleted" });
-      }
-    });
-  } catch (error) {
-    res.status(500).json({
-      error: error.message,
-    });
-  }
-};
-
-export const createMovie = async (req, res) => {
-  try {
-    const newMovie = new Movie(req.body);
-    await newMovie.save().then((movie) => {
-      res.status(201).json(movie);
-    });
-  } catch (error) {
-    res.status(500).json({
-      error: error.message,
-    });
-  }
-};
-
-export const getCommentById = async (req, res) => {
-  const movieId = req.params.movieId;
-  const commentId = req.params.commentId;
-
-  try {
-    const movie = await Movie.findById(movieId);
-    if (!movie) {
-      res.status(404).json({
-        message: "Movie not found",
-      });
-    } else {
-      const comment = movie.comments.find(
-        (comment) => comment._id == commentId
-      );
-      if (!comment) {
-        res.status(404).json({
-          message: "Comment not found",
+        res.status(200).json({
+          movie: movie,
         });
-      } else {
-        res.status(200).json(comment);
       }
-    }
-  } catch (error) {
-    res.status(500).json({
-      error: error.message,
     });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
-export const getCommentsByMovieId = async (req, res) => {
-  try {
-    const movieId = req.params.movieId;
-    const movie = await Movie.findById(movieId);
-    if (!movie) {
-      res.status(404).json({
-        message: "Movie not found",
-      });
-    } else if (!movie.comments.length) {
-      res.status(404).json({
-        message: "Comments not found",
-      });
-    } else {
-      res.status(200).json(movie.comments);
-    }
-  } catch (error) {
-    res.status(500).json({
-      error: error.message,
-    });
-  }
-};
-export const postComment = async (req, res) => {
-  try {
-    const movieId = req.params.movieId;
 
-    const comment = {
-      _id: new mongoose.Types.ObjectId(),
-      isAdmin: req.body.isAdmin,
-      firstname: req.body.firstname,
-      comment: req.body.comment,
-      like: 0,
-      dislike: 0,
+export const createMovie = (req, res) => {
+  try {
+    const movie = {
+      title: {
+        uz: req.body.title.uz,
+        ru: req.body.title.ru,
+        en: req.body.title.en,
+      },
+      notes: {
+        uz: req.body.notes.uz,
+        ru: req.body.notes.ru,
+        en: req.body.notes.en,
+      },
+      description: {
+        uz: req.body.description.uz,
+        ru: req.body.description.ru,
+        en: req.body.description.en,
+      },
+      releaseDate: {
+        day: req.body.releaseDate.day,
+        month: req.body.releaseDate.month,
+        year: req.body.releaseDate.year,
+      },
+      duration: {
+        hour: req.body.duration.hour,
+        min: req.body.duration.min,
+      },
+      rating: {
+        like: req.body.rating.like,
+        dislike: req.body.rating.dislike,
+        imdb: req.body.rating.imdb,
+      },
+      country: {
+        uz: req.body.country.uz,
+        ru: req.body.country.ru,
+        en: req.body.country.en,
+      },
+      credit: {
+        uz: req.body.credit.uz,
+        ru: req.body.credit.ru,
+        en: req.body.credit.en,
+      },
+      image: {
+        portrait: req.body.image.portrait,
+        fullscreen: req.body.image.fullscreen,
+      },
+      status: {
+        isNew: req.body.status.isNew,
+        isTrending: req.body.status.isTrending,
+        type: req.body.status.type,
+        isAvailable: req.body.status.isAvailable,
+      },
+      movie: {
+        uz: req.body.movie.uz,
+        ru: req.body.movie.ru,
+        en: req.body.movie.en,
+      },
+      trailer: req.body.trailer,
+      comments: [],
     };
-    const movie = await Movie.findById(movieId);
-    if (!movie) {
-      res.status(404).json({
-        message: "Movie not found",
-      });
-    } else if (!comment.comment.trim() || !comment.comment) {
-      res.status(404).json({
-        message: "Comment cannot be empty",
-      });
-    } else {
-      movie.comments.push(comment);
-      await movie.save();
-      res.status(201).json({
-        message: "Comment posted successfully",
-        movie: movie,
-        comment: comment,
-      });
-    }
-  } catch (error) {
-    res.status(500).json({
-      error: error.message,
-    });
-  }
-};
-export const deleteComment = async (req, res) => {
-  try {
-    const movieId = req.params.movieId;
-    const commentId = req.params.commentId;
-    const movie = await Movie.findById(movieId);
-    if (!movie) {
-      res.status(404).json({
-        message: "Movie not found",
-      });
-    } else {
-      movie.comments = movie.comments.filter(
-        (comment) => comment._id != commentId
-      );
-      await movie.save();
-      res.status(201).json(movie);
-    }
-  } catch (error) {
-    res.status(500).json({
-      error: error.message,
-    });
-  }
-};
-export const updateComment = async (req, res) => {
-  try {
-    const movieId = req.params.movieId;
-    const commentId = req.params.commentId;
-    const movie = await Movie.findById(movieId);
-    if (!movie) {
-      res.status(404).json({
-        message: "Movie not found",
-      });
-    } else {
-      const comment = movie.comments.find(
-        (comment) => comment._id == commentId
-      );
-      if (!comment) {
+    movies.insert(movie, (err, movie) => {
+      if (!movie) {
         res.status(404).json({
-          message: "Comment not found",
+          message: "Movie not found",
         });
       } else {
-        comment.firstname = req.body.firstname;
-        comment.comment = req.body.comment;
-        comment.like = req.body.like;
-        comment.dislike = req.body.dislike;
-        await movie.save();
-        res.status(201).json({ message: "Comment updated successfully" });
+        res.status(200).json({
+          movie: movie,
+        });
       }
-    }
-  } catch (error) {
-    res.status(500).json({
-      error: error.message,
     });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getCommentsByMovieId = (req, res) => {
+  try {
+    const id = req.params.movieId;
+    movies.findOne({ _id: id }, (err, movie) => {
+      if (!movie) {
+        res.status(404).json({
+          message: "Movie not found",
+        });
+      } else {
+        res.status(200).json({
+          comments: movie.comments,
+        });
+      }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getCommentById = (req, res) => {
+  try {
+    const movieId = req.params.movieId;
+    const commentId = req.params.commentId;
+    movies.findOne({ _id: movieId }, (err, movie) => {
+      if (!movie) {
+        res.status(404).json({
+          message: "Movie not found",
+        });
+      } else {
+        res.status(200).json({
+          comment: movie.comments[commentId],
+        });
+      }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const postComment = (req, res) => {
+  try {
+    const movieId = req.params.movieId;
+    const comment = req.body;
+    movies.update(
+      { _id: movieId },
+      { $push: { comments: comment } },
+      (err, movie) => {
+        if (!movie) {
+          res.status(404).json({
+            message: "Movie not found",
+          });
+        } else {
+          res.status(200).json({
+            movie: movie,
+          });
+        }
+      }
+    );
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const deleteComment = (req, res) => {
+  try {
+    const movieId = req.params.movieId;
+    const commentId = req.params.commentId;
+    movies.update(
+      { _id: movieId },
+      { $pull: { comments: { _id: commentId } } },
+      (err, movie) => {
+        if (!movie) {
+          res.status(404).json({
+            message: "Movie not found",
+          });
+        } else {
+          res.status(200).json({
+            movie: movie,
+          });
+        }
+      }
+    );
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const updateComment = (req, res) => {
+  try {
+    const movieId = req.params.movieId;
+    const commentId = req.params.commentId;
+    const comment = req.body;
+    movies.update(
+      { _id: movieId, "comments._id": commentId },
+      { $set: { "comments.$": comment } },
+      (err, movie) => {
+        if (!movie) {
+          res.status(404).json({
+            message: "Movie not found",
+          });
+        } else {
+          res.status(200).json({
+            movie: movie,
+          });
+        }
+      }
+    );
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
