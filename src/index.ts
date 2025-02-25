07e355937226f5b7127cb74b77756173b2c513fc
Usmon Reyimberganov <usmonw@icloud.com>
2025-02-25 23:25:09 +0500

    fixing au

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import "dotenv/config";
 import apiKeyMiddleware from "./middlewares/apiKey.middleware";
 import userRouter from "./routes/users.routes";
 import { proxy } from "./controllers/playerProxy.controller";
+import { createProxyMiddleware } from "http-proxy-middleware";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -12,8 +14,31 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-app.use("/vidsrc", proxy);
-
+app.use(cookieParser());
+app.use("/vidsrc", (req, res, next) => {
+  req.headers["cookie"] = req.headers["cookie"] || "";
+  next();
+});
+app.use(
+  "/vidsrc",
+  createProxyMiddleware({
+    target: "https://vidsrc.xyz",
+    changeOrigin: true,
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+      Referer: "https://vidsrc.xyz/",
+    },
+    onProxyReq: function (
+      proxyReq: import("http").ClientRequest,
+      req: express.Request,
+      res: express.Response
+    ) {
+      proxyReq.setHeader("User-Agent", req.headers["user-agent"] || "");
+      proxyReq.setHeader("Referer", "https://vidsrc.xyz/");
+    },
+  } as any)
+);
 app.use(apiKeyMiddleware);
 app.get("/", apiKeyMiddleware, (req, res) => {
   res.json({ message: "Welcome to the Smile Movies API" });
